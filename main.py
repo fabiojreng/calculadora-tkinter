415dc97069b327a961b307ab0f0bcd3a3b7353d8
Fábio Júnior <fabiojunioreloi@hotmail.com>
2023-12-10 09:17:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Calculadora(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -8,23 +9,42 @@ class Calculadora(tk.Tk):
         self.resizable(True, True)
         self.maxsize(400, 500)
         self.minsize(250, 300)
-        self.bind("<Key>", self.on_key_press)# manipulador de eventos para as teclas
+        self.bind("<Key>", self.on_key_press)  # manipulador de eventos para as teclas
         self.result_var = tk.StringVar()
         self.create_widgets()
 
     def create_widgets(self):
         # Entrada para exibir os números e resultados
-        entry = tk.Entry(self, textvariable=self.result_var, font=('Arial', 18), bd=10, relief=tk.GROOVE, justify="right")
+        entry = tk.Entry(
+            self,
+            textvariable=self.result_var,
+            font=("Arial", 18),
+            bd=10,
+            relief=tk.GROOVE,
+            justify="right",
+        )
         entry.grid(row=0, column=0, columnspan=4)
 
         # Botões
         buttons = [
-            '7', '8', '9', '/',
-            '4', '5', '6', '*',
-            '1', '2', '3', '-',
-            '0', '.', '=', '+'
+            "7",
+            "8",
+            "9",
+            "/",
+            "4",
+            "5",
+            "6",
+            "*",
+            "1",
+            "2",
+            "3",
+            "-",
+            "0",
+            ".",
+            "=",
+            "+",
         ]
-        
+
         # Configuração da grade para expandir os botões com o tamanho da janela
         for i in range(4):
             self.grid_columnconfigure(i, weight=1)
@@ -34,10 +54,26 @@ class Calculadora(tk.Tk):
         col_val = 0
 
         for button in buttons:
-            if button in ['/', '*', '-', '+', '=']:
-                tk.Button(self, text=button, padx=20, pady=20, font=('Arial', 14), command=lambda b=button: self.on_button_click(b), bg='orange').grid(row=row_val, column=col_val, sticky="nsew")
+            if button in ["/", "*", "-", "+", "="]:
+                tk.Button(
+                    self,
+                    text=button,
+                    padx=20,
+                    pady=20,
+                    font=("Arial", 14),
+                    command=lambda b=button: self.on_button_click(b),
+                    bg="orange",
+                ).grid(row=row_val, column=col_val, sticky="nsew")
             else:
-                tk.Button(self, text=button, padx=20, pady=20, font=('Arial', 14), command=lambda b=button: self.on_button_click(b)).grid(row=row_val, column=col_val, sticky="nsew")
+                tk.Button(
+                    self,
+                    text=button,
+                    padx=20,
+                    pady=20,
+                    font=("Arial", 14),
+                    command=lambda b=button: self.on_button_click(b),
+                ).grid(row=row_val, column=col_val, sticky="nsew")
+
             col_val += 1
             if col_val > 3:
                 col_val = 0
@@ -46,7 +82,7 @@ class Calculadora(tk.Tk):
     def on_button_click(self, button):
         current_text = self.result_var.get()
 
-        if button == '=':
+        if button == "=":
             try:
                 result = eval(current_text)
                 self.result_var.set(result)
@@ -58,8 +94,9 @@ class Calculadora(tk.Tk):
     def on_key_press(self, event):
         key = event.char
 
-        if key.isdigit() or key in ['/', '*', '-', '+', '=', '.', "/r"]:
+        if key.isdigit() or key in ["/", "*", "-", "+", "=", ".", "/r"]:
             self.on_button_click(key)
+
 
 if __name__ == "__main__":
     app = Calculadora()

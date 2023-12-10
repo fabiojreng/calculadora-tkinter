603705a109f92cc06a569be4efff770158c9b727
Fábio Júnior <fabiojunioreloi@hotmail.com>
2023-12-10 09:53:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ class Calculadora(tk.Tk):
         self.resizable(True, True)
         self.maxsize(400, 500)
         self.minsize(250, 300)
-        self.bind("<Key>", self.on_key_press)  # manipulador de eventos para as teclas
+        self.bind("<Key>", self.on_key_press)
         self.result_var = tk.StringVar()
         self.create_widgets()
 
     def create_widgets(self):
-        # Entrada para exibir os números e resultados
         entry = tk.Entry(
             self,
             textvariable=self.result_var,
@@ -25,7 +24,6 @@ class Calculadora(tk.Tk):
         )
         entry.grid(row=0, column=0, columnspan=4)
 
-        # Botões
         buttons = [
             "7",
             "8",
@@ -45,39 +43,31 @@ class Calculadora(tk.Tk):
             "+",
         ]
 
-        # Configuração da grade para expandir os botões com o tamanho da janela
         for i in range(4):
             self.grid_columnconfigure(i, weight=1)
             self.grid_rowconfigure(i + 1, weight=1)
 
-        row_val = 1
-        col_val = 0
+        row_val, col_val = 1, 0
 
         for button in buttons:
-            if button in ["/", "*", "-", "+", "="]:
-                tk.Button(
-                    self,
-                    text=button,
-                    padx=20,
-                    pady=20,
-                    font=("Arial", 14),
-                    command=lambda b=button: self.on_button_click(b),
-                    bg="orange",
-                ).grid(row=row_val, column=col_val, sticky="nsew")
-            else:
-                tk.Button(
-                    self,
-                    text=button,
-                    padx=20,
-                    pady=20,
-                    font=("Arial", 14),
-                    command=lambda b=button: self.on_button_click(b),
-                ).grid(row=row_val, column=col_val, sticky="nsew")
-
+            self.create_button(button, row_val, col_val)
             col_val += 1
             if col_val > 3:
                 col_val = 0
                 row_val += 1
+
+    def create_button(self, text, row, col):
+        bg_color = "orange" if text in ["/", "*", "-", "+", "="] else None
+        button = tk.Button(
+            self,
+            text=text,
+            padx=20,
+            pady=20,
+            font=("Arial", 14),
+            command=lambda t=text: self.on_button_click(t),
+            bg=bg_color,
+        )
+        button.grid(row=row, column=col, sticky="nsew")
 
     def on_button_click(self, button):
         current_text = self.result_var.get()
@@ -93,8 +83,7 @@ class Calculadora(tk.Tk):
 
     def on_key_press(self, event):
         key = event.char
-
-        if key.isdigit() or key in ["/", "*", "-", "+", "=", ".", "/r"]:
+        if key.isdigit() or key in ["/", "*", "-", "+", "=", "."]:
             self.on_button_click(key)
 
 
